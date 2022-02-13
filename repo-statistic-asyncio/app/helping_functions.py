627db63a3db9fdbe6235c1434ba051e12bbf25a5
Vsevolod Tetervak <vsevolod.tetervak@playrix.com>
2022-02-13 17:58:12 +0500

    chore: removed time period string function and test

--- repo-statistic-asyncio/app/helping_functions.py
+++ repo-statistic-asyncio/app/helping_functions.py
@@ -1,23 +1,6 @@
 from datetime import datetime
-from typing import Optional
 
 from app.exceptions import DateArgumentException
-
-
-def get_time_period_string(
-        since: Optional[datetime] = None,
-        until: Optional[datetime] = None,
-) -> str:
-    match (since, until):
-        case (None, None):
-            return "for the all time"
-        case (date1, None):
-            return f"from {date1} to now"
-        case (None, date2):
-            return f"from the beginning to {date2}"
-        case (date1, date2):
-            return f"from {date1} to {date2}"
-    raise ValueError()
 
 
 def get_argument_date(date_str: str) -> datetime:
--- repo-statistic-asyncio/test/test_helping_functions.py
+++ repo-statistic-asyncio/test/test_helping_functions.py
@@ -3,7 +3,6 @@ import pytest
 
 from app.helping_functions import (
     get_argument_date,
-    get_time_period_string,
 )
 from app.exceptions import DateArgumentException
 
@@ -27,17 +26,4 @@ def test_get_argument_date(arg, expected):
 ))
 def test_get_argument_date_raises(arg):
     with pytest.raises(DateArgumentException):
-        get_argument_date("asdasdas")
-
-
-@pytest.mark.parametrize("arg1, arg2, expected", (
-    (ex1, ex2, f"from {ex1} to {ex2}"),
-    (ex1, None, f"from {ex1} to now"),
-    (None, ex2, f"from the beginning to {ex2}"),
-    (None, None, "for the all time"),
-))
-def test_get_time_period_string(arg1, arg2, expected):
-    assert expected == get_time_period_string(
-        since=arg1,
-        until=arg2,
-    )
+        get_argument_date(arg)
